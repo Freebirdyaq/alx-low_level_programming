59ff15f27a3ba1c1035e60913727b1c5514bf821
freebirdyaq <elizabethyaquo@gmail.com>
2022-04-19 18:36:38 +0000

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,17 +16,17 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 return (0);
 
-if (n > 0)
+if (n == 0)
 {
-        printf("%i is positive\n", n);
+printf("%d is zero\n", n);
 }
-else if (n == 0)
+else if (n > 0)
 {
-	printf("%i is zero\n", n);
+printf("%i is positive\n", n);
 }
 else
 {
-	printf("%i is negative\n", n);
+printf("%i is negative\n", n);
 }
 return (0);
 }
