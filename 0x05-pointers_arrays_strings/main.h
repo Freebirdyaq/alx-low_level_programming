15df95ac0b0137a9c6e3d5d47924eedfc5c7336f
freebirdyaq <elizabethyaquo@gmail.com>
2022-04-21 13:41:18 +0000

    98 Battery st

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,6 @@
-int putchar(int char);
+#include <stdio.h>
+
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -9,4 +11,3 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-
