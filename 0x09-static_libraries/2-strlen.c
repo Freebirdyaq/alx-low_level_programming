6966613a25a815939822408e3b9994dbd8c56fce
freebirdyaq <elizabethyaquo@gmail.com>
2022-05-03 23:02:48 +0000

    static library containing all functions

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: input string.
+ * Return: length of a string.
+ */
+
+int _strlen(char *s)
+{
+	int x = 0;
+
+	while (*(s + x) != '\0')
+		x++;
+	return (x);
+}
